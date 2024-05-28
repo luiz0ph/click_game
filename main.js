76cfe55a801aeb293a0a651ca9430763d145f766
Luiz Phelipe <luizphsantana@gmail.com>
2024-05-28 20:09:48 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+let score = 96;
+let dobro_pontos = document.getElementById('dobro_pontos');
+let btn_dobroPontos = document.querySelector('button#btn_dobro');
+let compras = document.querySelector('div#compras');
+let dobroPonto = false;
+
+// Função de dobro de ponto
+function dobro() {
+    if (score < 100) { 
+        window.alert('Valor Insuficiente');
+    } else {
+        dobroPonto = true;
+        score -= 100; // Retira 100 pontos
+        document.getElementById('score').innerHTML = score;
+        compras.style.display = 'none';
+    }
+}
+
+btn_dobroPontos.addEventListener('click', dobro);
+
+// Função para atualizar a pontuação
+function atualizarPontuacao() {
+    if (dobroPonto == true) {
+        score += 2;
+    } else {
+        score++;
+    }
+    document.getElementById('score').innerHTML = score;
+}
+
+// Chama a função atualizarPontuacao para iniciar o processo
+atualizarPontuacao();
